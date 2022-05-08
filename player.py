b432673505194681c22dd3c200b1eabfb53a747b
Jordan <Jordforb@gmail.com>
2022-05-08 05:00:16 -0400

    created languages

--- player.py
+++ player.py
@@ -1,11 +1,39 @@
+langs={
+    "unit":{
+    "initialize":"*BREEN* initializing unit",
+    "activate":"BEEP BEEP Unit activated",
+    "vitals":"--getting vital signs--",
+    "hp":"BZZZT my HP is now:",
+    "damage":"BJORK I HAVE BEEN HIT FOR",
+    "hpNull":"SYSTEM FAILURE",
+    "die":"BOOOOM!"
+    },
+    "enemy":{
+    "initialize":"KRSH KRSH Enemy Unit Activated",
+    "activate":"SKRONK Enemy Spotted",
+    "vitals":">>>scanning Enemy>>>",
+    "hp":"Enemy Health is:",
+    "damage":"SHWING! I have dealt the enemy",
+    "hpNull":"YES! ENEMY SYSTEMS FAILING",
+    "die":"KRAAASH the enemy has fallen!"
+    }
+}
+
 class Player():
-    def __init__(self):
+    def __init__(self,lang=langs["unit"],hp=100):
+        self.baseHP= hp
         self.hp= 0
+        self.lang=lang
+        self.speak("initialize")
         self.activate()
-        
+    
+    def speak(self,key):
+        print(self.lang[key])
+        return self 
+    
     def activate(self):
-        self.setHP(100)
-        print(f"BEEP BEEP Unit activated with {self.getHP()} health")
+        self.speak("activate")
+        self.setHP(self.baseHP)
         return self
         
     def getHP(self):
@@ -13,12 +41,49 @@ class Player():
     
     def setHP(self,hp):
         self.hp= hp
-        print(f"BZZZZT my HP is now {self.getHP()}")
+        self.speak("hp")
+        print(f"--{self.getHP()} HP--")
         return self 
     
     def takeDamage(self,dmg):
+        self.speak("damage")
+        print(f'{dmg} damage!')
+        hp = self.getHP() - dmg 
+        self.setHP(hp)
+        if self.lifeCheck() == True:
+            return self
+        else:
+            self.die()
+    
+    def dealDamage(self,target,dmg):
+        target.takeDamage(dmg)
         
+    def lifeCheck(self):
+        self.speak("vitals")
+        if self.getHP()<= 0:
+            self.speak("hpNull")
+            return False
+        else:
+            return True
+        
+    def die(self):
+        self.speak("die")
+        
+class Enemy(Player):
+    def __init__(self,hp=30,lang=langs["enemy"]):
+        print("ENEMY ROBOT APPROACHES")
+        self.baseHP=hp
+        self.lang=lang
+        self.hp=0
+        self.activate()
+ 
         
 
 u= Player()
-u.setHP(300)
+e= Enemy()
+u.dealDamage(e,5)
+e.dealDamage(u,4)
+u.takeDamage(500)
+e.takeDamage(400)
+
+# e.activate()
