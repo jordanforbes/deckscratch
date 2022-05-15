0d294a9f11e2f22b2c1c252eaeae793762787e90
Jordan <Jordforb@gmail.com>
2022-05-15 12:57:27 -0400

    added login

--- flasktut/main.py
+++ flasktut/main.py
@@ -1,11 +1,17 @@
-import re
-from flask import Flask, template_rendered, render_template
+from flask import Flask, render_template,request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html",content="it works")
+
+@app.route("/login/", methods=["POST","GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["usrname"]
+        return redirect(url_for("usr", usr = user))
+    return render_template("form.html")
 
 @app.route("/<usr>")
 def usr(usr):
